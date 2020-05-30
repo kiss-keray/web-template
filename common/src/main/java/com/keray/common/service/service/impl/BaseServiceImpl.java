784ce23b328aa2af72ebe394ee92c88c2388f110
keray <11723046452qq.com>
2020-05-30 16:57:41 +0800

    还原BaseServiceImpl

---
+++ common/src/main/java/com/keray/common/service/service/impl/BaseServiceImpl.java
@@ -0,0 +1,12 @@
+package com.keray.common.service.service.impl;
+
+import com.keray.common.BaseEntity;
+import com.keray.common.BaseService;
+
+/**
+ * @author by keray
+ * date:2020/5/30 4:55 下午
+ */
+public abstract class BaseServiceImpl<T extends BaseEntity> implements BaseService<BaseEntity> {
+
+}
