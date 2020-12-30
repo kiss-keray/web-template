7fa9aa6aa0283e6d31518d28dfb672a12e7f275d
keray <11723046452qq.com>
2020-12-30 18:06:36 +0800

    线程池

--- common/src/main/java/com/keray/common/SysThreadPool.java
+++ common/src/main/java/com/keray/common/SysThreadPool.java
@@ -2,19 +2,22 @@ package com.keray.common;
 
 import java.util.Map;
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author by keray
  * date:2019/9/16 11:49
  */
 public class SysThreadPool {
-    private static final ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(100, 1000, 10,
-            TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>(10000000),
+    private static final AtomicInteger COUNT = new AtomicInteger(0);
+    private static final ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(500, 1000, 10,
+            TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>(100000),
             r -> {
                 Thread t = new Thread(r);
-                t.setName("sys-thread");
+                t.setName("sys-thread-" + COUNT.getAndIncrement());
                 return t;
             });
+
     private volatile static IUserContext userContext = null;
 
     public static void execute(Runnable runnable) {
